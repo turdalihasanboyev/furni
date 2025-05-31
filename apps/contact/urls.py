242687833a85adad4df6af3510ca66f5fd3ef8dd
Turdali <turdalihasanboyev144@gmail.com>
2025-05-31 19:16:32 +0500

    writing contact page

--- apps/contact/urls.py
+++ apps/contact/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
+from .views import ContactPageView
+
 
 urlpatterns = [
-    
+    path('contact/', ContactPageView.as_view(), name='contact'),
 ]
--- apps/contact/views.py
+++ apps/contact/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from django.views import View
+
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+
+from django.contrib import messages
+
+from .models import Contact
+from apps.common.models import SubEmail
+
+
+@method_decorator(login_required, name='dispatch')
+class ContactPageView(View):
+    def get(self, request):
+        return render(request, 'contact.html')
+
+    def post(self, request):
+        name = request.POST.get('name')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+
+        if first_name and last_name and email and message:
+            contact = Contact()
+            contact.first_name = first_name
+            contact.last_name = last_name
+            contact.email = email
+            contact.message = message
+            contact.save()
+            messages.success(request, 'Your message has been sent successfully.')
+            return redirect('contact')
+
+        if name and email:
+            sub_email = SubEmail()
+            sub_email.name = name
+            sub_email.email = email
+            sub_email.save()
+            messages.success(request, 'Your email has been added successfully.')
+            return redirect('contact')
