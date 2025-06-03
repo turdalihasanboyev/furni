373000205028223a2abd09f09a95b5ca8bd03efe
Turdali <turdalihasanboyev144@gmail.com>
2025-06-03 18:20:50 +0500

    ```add docker```

--- config/settings.py
+++ config/settings.py
@@ -132,7 +132,7 @@ DATABASES = {
         'NAME': 'furni',
         'USER': 'furni',
         'PASSWORD': 'furni',
-        'HOST': '127.0.0.1',
+        'HOST': 'db',
         'PORT': '5432',
     }
 }
